8086b09cbafa13f1b7544f47d36cd585016ae2fa
AhmedNagy1911 <zamalekahmed196>
2026-03-05 03:20:58 +0200

    add api

--- MechanicShop.Api/Program.cs
+++ MechanicShop.Api/Program.cs
@@ -1,10 +1,24 @@
+using MechanicShop.Api.Components;
+using MechanicShop.Infrastructure.Data;
+using MechanicShop.Infrastructure.RealTime;
+
+using Scalar.AspNetCore;
+
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services
+    .AddPresentation(builder.Configuration)
+    .AddApplication()
+    .AddInfrastructure(builder.Configuration);
+
+builder.Host.UseSerilog((context, loggerConfig) =>
+    loggerConfig.ReadFrom.Configuration(context.Configuration));
 
 var app = builder.Build();
 
@@ -12,12 +26,39 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "MechanicShop API V1");
+
+        options.EnableDeepLinking();
+        options.DisplayRequestDuration();
+        options.EnableFilter();
+    });
+
+    app.MapScalarApiReference();
+
+    await app.InitialiseDatabaseAsync();
+
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseCoreMiddlewares(builder.Configuration);
 
 app.MapControllers();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+
+app.MapRazorComponents<App>().AllowAnonymous()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(MechanicShop.Client._Imports).Assembly);
+
+app.MapHub<WorkOrderHub>("/hubs/workorders");
 
 app.Run();
